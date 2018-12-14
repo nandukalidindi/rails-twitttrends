03779873fa4cfa78392f1893fe6bf791d53c90db
nandukalidindi <nandukalidindi@gmail.com>
2018-12-13 22:24:15 -0500

    Push the final changes before archiving the repository from local

--- sample_kafka_consumer.rb
+++ sample_kafka_consumer.rb
@@ -5,3 +5,11 @@ kafka = Kafka.new(seed_brokers: ["localhost:9092"])
 kafka.each_message(topic: "greetings") do |message|
   puts message.offset, message.key, message.value
 end
+
+require "kafka"
+
+kafka = Kafka.new(seed_brokers: ["localhost:9092", "ec2-34-201-118-65.compute-1.amazonaws.com:9092"])
+
+kafka.each_message(topic: "test") do |message|
+  puts message.offset, message.key, message.value
+end
--- sample_kafka_producer.rb
+++ sample_kafka_producer.rb
@@ -6,3 +6,13 @@ kafka = Kafka.new(
 )
 
 kafka.deliver_message("Hello, World!", topic: "greetings")
+
+
+require "kafka"
+
+kafka = Kafka.new(
+  seed_brokers: ["localhost:9092", "ec2-34-201-118-65.compute-1.amazonaws.com:9092"],
+  client_id: "my-application"
+)
+
+kafka.deliver_message("Hello, World!", topic: "test")
